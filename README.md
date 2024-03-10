b2a1e9040098083b1658b2bfbad400e6a63abf2e
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2024-03-10 17:40:02 +0900

    修正

---
+++ README.md
@@ -0,0 +1,3 @@
+# msx_hello_world_c
+
+Project template for cross compiling MSX software on macOS.
--- msx-toolchain.cmake
+++ msx-toolchain.cmake
@@ -1,7 +1,12 @@
-SET(CMAKE_SYSTEM_NAME MSX)
-SET(CMAKE_C_COMPILER zcc)
-SET(CMAKE_ASM_COMPILER zcc)
+set(CMAKE_SYSTEM_NAME MSX)
+# set(CMAKE_SYSTEM_PROCESSOR z80)
+
+set(Z88DK_ROOT $ENV{HOME}/bin/z88dk)
+
+set(CMAKE_C_COMPILER ${Z88DK_ROOT}/bin/zcc)
+set(CMAKE_ASM_COMPILER ${Z88DK_ROOT}/bin/zcc)
 
 set(CMAKE_C_FLAGS "+msx -subtype=msxdos")
 set(CMAKE_CXX_FLAGS "+msx -subtype=msxdos")
 
+include_directories(${Z88DK_ROOT}/include)
